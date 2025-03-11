9cc18388ab40f58032f45b2d171905ae60d89715
SIIM_SILLAND <siim.silland@gmail.com>
2025-03-11 21:28:36 +0200

    StudentProfileView.vue

--- src/views/StudentProfileView.vue
+++ src/views/StudentProfileView.vue
@@ -1,46 +1,56 @@
 <template>
- <div v-if="isStudent">
-  <div class="row">
-    <div class="col">
-      <h1>SIIA ÕPILASE NIMI!</h1>
-    </div>
-  </div>
-  <div>
-    <div class="container text-center">
-      <div class="row align-items-start">
-        <div class="col">
-          EELISTUSED ASUKOHA OSAS
-        </div>
-        <div class="col">
-          Lühitutvustus
-
-        </div>
-        <div class="col">
-          SIIA PILT JA MUUD ISIKLIKUD ANDMED
-        </div>
+  <div v-if="isStudent">
+    <div class="row">
+      <div class="col">
+        <h1> {{ studentProfile.firstName }} {{ studentProfile.lastName }}</h1>
       </div>
     </div>
+    <div>
+      <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col">
+            EELISTUSED ASUKOHA OSAS
+          </div>
+          <div class="col">
+            Lühitutvustus
 
-  </div>
-  <div>
-    <div class="container text-center">
-      <div class="row align-items-start">
-        <div class="col">
-          EELISTUSED PRAKTIKA ALGUSE OSAS
-        </div>
-        <div class="col">
+          </div>
+          <div class="col">
+            PILT
 
-          (SIIA LÜHITUTVUSTUS)
-
-        </div>
-        <div class="col">
-          SIIA PILT JA MUUD ISIKLIKUD ANDMED
+          </div>
         </div>
       </div>
-    </div>
 
+    </div>
+    <div>
+      <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col">
+            <h5>EELISTUSED PRAKTIKA ALGUSE OSAS</h5>
+          </div>
+          <div class="col">
+
+            (SIIA LÜHITUTVUSTUS)
+
+          </div>
+          <div class="col">
+            <h3>MUUD ISIKLIKUD ANDMED</h3>
+            <ul class="list-group list-group-horizontal">
+              <li class="list-group-item">Aadress </li>
+              <li class="list-group-item">
+                {{studentProfile.address}}
+              </li>
+
+            </ul>
+            <h3> PROOVIKAS teise põhja jaoks </h3>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
- </div>
   <div v-else class="unauthorized-message">
     <h2> Juuredepääs keelatud </h2>
     <p>Teil pole selle lehe vaatamiseks õiguse.</p>
@@ -52,32 +62,78 @@
 
 
 <script>
+
+import axios from "axios";
+
 export default {
   name: 'StudentProfileView',
 
   data() {
     return {
-      isStudent: false
+      isStudent: false,
+
+      studentProfile: {
+        firstName: '',
+        lastName: '',
+        address: '',
+        phone: '',
+        linkedin: '',
+        email: ''
+      },
+      error: null
+
     }
 
   },
 
-  mounted(){
-    this.checkUserRole()
+  mounted() {
+    this.checkUserRole();
+    if (this.isStudent)
+    {
+      this.fetchStudentProfile();
+    }
   },
 
-  methods:{
-    checkUserRole(){
+  methods: {
+    //todo: pildi lisamine
+
+    checkUserRole() {
       const roleName = sessionStorage.getItem('roleName')
       this.isStudent = roleName === 'student'
 
-      if (!this.isStudent){
+      if (!this.isStudent) {
         setTimeout(() => {
           this.$router.push('/')
         }, 2000)
 
       }
+    },
+
+    fetchStudentProfile() {
+      const userId = sessionStorage.getItem('userId');
+      if (!userId) {
+        this.error = 'User ID not found in session';
+      }
+      axios.get('/student/profile/get', {
+        params: {
+          userId: userId
+        }
+      })
+          .then(response => {
+            this.studentProfile = response.data;
+          })
+          .catch(error => {
+            console.error('Error fetching student profile: ', error);
+            this.error = 'Failed to load student profile';
+          })
+
+    },
+
+    // todo: miks beforeMount ei rakendu?
+    beforeMount() {
+      this.checkUserRole();
     }
+
   }
 }
 
