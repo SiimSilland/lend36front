0627688a6aecd1132d6b9a7a498e60798b4cf64a
kallebammer <kalleb49@gmail.com>
2025-03-12 09:16:25 +0200

    Company register view

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -5,16 +5,17 @@ export default {
     sendPostNewStudentRequest(newStudent) {
         return axios.post('/user', newStudent);
     },
-    sendPostNewCompanyProfileRequest(userId, companyDto)  {
-        return axios.put('/company/profile/update', companyDto,{
-            params: {
-            userId: userId
-        }
-    }
-    );
-    },
+
     sendPostNewCompanyRequest(newCompany) {
         return axios.post('/company/register', newCompany);
-    }
+    },
+    sendPostNewCompanyProfileRequest(userId, companyDto)  {
+        return axios.put('/company/profile/update', companyDto,{
+                params: {
+                    userId: userId
+                }
+            }
+        );
+    },
 }
 
