7d7d681f7185f7cec1a74c4170ab240963bd42ef
SIIM_SILLAND <siim.silland@gmail.com>
2025-03-13 09:51:54 +0200

    ErrorView NavigationService.js StudentProfileView.vue StudentEditProfileTable.vue

--- src/views/StudentProfileView.vue
+++ src/views/StudentProfileView.vue
@@ -112,31 +112,35 @@ export default {
     startEdit() {
       this.isEditMode = true
     },
-    async saveEdit() {
-      try {
-        const response = await StudentProfileService.updateStudentProfile(this.userId, this.studentProfile);
-        this.studentProfile = { ...response.data }; // Spread operator to ensure reactivity
-        this.isEditMode = false;
-      } catch (error) {
-        console.error('Error saving profile: ', error);
-        alert('Profiili salvestamisel tekkis viga');
-      }
+    saveEdit(){
+      StudentProfileService.updateStudentProfile(this.userId, this.studentProfile)
+          .then(response => {
+            this.isEditMode = false;
+            this.studentProfile = response.data;
+          })
+          .catch( error =>{
+            console.error('Error saving profile: ', error);
+            alert('Profiili salvestamisel tekkis vida');
+            //this.getStudentProfile();
+          });
+
     },
-    async getStudentProfile() {
-      try {
-        const response = await StudentProfileService.sendGetStudentProfile(this.userId);
-        this.studentProfile = { ...response.data }; // Spread operator to ensure reactivity
-      } catch (error) {
-        NavigationService.navigateToErrorView();
-      }
-    }
+
+    updateStudentProfile() {
+      StudentProfileService.updateStudentProfile(this.userId, this.studentProfile)
+
+    },
+
+    getStudentProfile() {
+      StudentProfileService.sendGetStudentProfile(this.userId)
+          .then(response => this.studentProfile = response.data)
+          .catch(() => NavigationService.navigateToErrorView());
+    },
   },
-  async created() {  // Changed from beforeMount to created
-    await this.getStudentProfile();
+  beforeMount() {
+    this.getStudentProfile()
+
   }
-
-
-
 }
 
 </script>
